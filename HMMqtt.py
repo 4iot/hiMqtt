e4f68fc67bb3bc77014dcaca10c1f28482bd3c6e
informix <eric.vercelletto@begooden-it.com>
2016-06-06 11:02:37 +0200

    fixed end of register

--- HMMqtt.py
+++ HMMqtt.py
@@ -4,6 +4,7 @@ import netinfo
 import os
 import json
 import re
+import shutil
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -15,15 +16,13 @@ global deviceId
 def on_message(client, userdata, msg):
     match = re.search('4iot/register_accepted',msg.topic)
     if ( match != None ):
-        print "Device accepted"
         match = re.search('device is now identified as \[(\w{33})',msg.payload)
         if ( match == None ):
             a = 1
         else:
             deviceId = match.group(1)
             idFileName = expanduser("~") + '/.hidevid'
-            fileExists = os.path.exists(idFileName)
-            if fileExists:
+            if ( os.path.exists(idFileName)):
                 # if file already exists, remove to be replaced
                 os.remove(idFileName)
 
@@ -31,7 +30,14 @@ def on_message(client, userdata, msg):
             idFileHandle = open(idFileName,'w')
             idFileHandle.write(deviceId)
             idFileHandle.close
-            mqttc.unsubscribe(expected_topic)
+            # copy file to /tmp for if any other user
+            idFileHandle = open('/tmp/.hidevid','w')
+            idFileHandle.write(deviceId)
+            idFileHandle.close
+            client.unsubscribe(msg.topic)
+            client.disconnect()
+
+            print "Device registered as " + deviceId
     else:
         print "Device rejected"
         exit(1)
@@ -46,35 +52,42 @@ def connectMqtt(Broker,Port,Keepalive):
     from os.path import expanduser
     deviceCacheFileName = expanduser("~") + '/.deviceCache'
     idFileName = expanduser("~") + '/.hidevid'
-    fileExists = os.path.exists(idFileName)
-    if ( fileExists ):
+    while  not (os.path.exists(idFileName)):
+        if ( os.path.exists('/tmp/.hidevid')):
+            # hi has been launched sometime by another user
+            # take /tmp/.hidevid and copy to user's home
+            idFileName = expanduser("~") + '/.hidevid'
+            shutil.copy('/tmp/.hidevid',idFileName)
+            idFileHandle = open(idFileName,'r')
+            deviceId = idFileHandle.readline()
+            deviceId = deviceId.strip()
+        else:
+            network = netinfo.get_network_interfaces()
+            networkMac = network['eth0']['mac']
+            if ( networkMac != None ):
+                client =  "4iot@" + networkMac + ":" + str(os.getpid())
+                clientId = client
+                deviceId = None
+                break
+
+    if ( os.path.exists(idFileName) ):
         idFileHandle = open(idFileName,'r')
         deviceId = idFileHandle.readline()
         deviceId = deviceId.strip()
         idFileHandle.close
-    else:
-        network = netinfo.get_network_interfaces()
-        networkMac = network['eth0']['mac']
-        if ( networkMac != None ):
-            client =  "4iot@" + networkMac + ":" + str(os.getpid())
-            clientId = client
-            deviceId = None
-
-
-    if ( deviceId == None and clientId != None ):
+    elif ( deviceId == None and clientId != None ):
         expected_topic = "4iot/register_accepted/" + clientId
         mqttc = mqttcl.Client(client)
         mqttc.on_connect = on_connect
         mqttc.on_message = on_message
         mqttc.connect(broker,port,keepalive)
         mqttc.subscribe(expected_topic,2)
-        mqttc.publish("4iot/please_register", clientId,1)
+        mqttc.publish("4iot/please_register", clientId,2)
         mqttc.loop_forever()
     else:
-        a = 1   
+        print ('No way to register, sorry!')
         
 def publish_message(client,message):
     mqttc = mqttcl.Client(client)
     mqttc.connect(broker,port,keepalive)
     mqttc.publish("4iot/message", json.dumps(message),1)
-    mqttc.loop(timeout=1.0, max_packets=1)
